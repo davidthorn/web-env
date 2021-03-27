908ec47cb9f0da8fc35198fabb0b63510841a5c0
David Thorn <david.thorn@springlane.de>
2021-03-27 08:45:40 +0100

    Added new install script command

---
+++ install.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+
+docker-compose build --no-cache
+
+docker-compose up -d
+
+docker exec -it sw_webphp chmod -R 755 custom/plugins
+docker exec -it sw_webphp chmod -R 755 engine/Shopware/Plugins/Community
+docker exec -it sw_webphp chmod -R 755 files
+docker exec -it sw_webphp chmod -R 755 media
+docker exec -it sw_webphp chmod -R 755 var
+docker exec -it sw_webphp chmod -R 755 web
+
+docker exec -it sw_webphp make init
