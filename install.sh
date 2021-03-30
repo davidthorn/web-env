32df3c67bb53a099d3f432ec55cbb40f7e98e2c3
David Thorn <david.thorn@springlane.de>
2021-03-30 15:34:01 +0200

    Updated install.sh and added .env.example The install.sh script now downloads the basic-plugin and then installs it to craft cms.

--- install.sh
+++ install.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+docker-compose down \
+                --remove-orphans \
+                # --rmi=all # uncomment if you want a super-clean-install by removing all images.
+
+rm -rf src/vendor
+rm -rf src/.env
+rm -rf src/plugins
+
+cp src/.env.example src/.env
+
+if [ ! -d "src/plugins" ]; then
+    mkdir src/plugins
+fi
+
+if [ ! -d "src/plugins/basic-plugin" ]; then
+    git clone git@deploy.office.springlane.de:backend-modules/base-craft-plugin.git --branch=development src/plugins/basic-plugin
+    cd src
+    composer require springlane/basic-plugin
+    cd ..
+fi
+
 docker-compose build --no-cache
 
 docker-compose up -d
 
 docker exec -it craftcms_webphp composer install
 
-docker exec -it craftcms_webphp php ./craft install/craft --interactive=0 --username=admin --site-url=http://localhost:8082 --site-name="Springlane Magazine" --password=password --language=en --email=backend@springlane.de
+docker exec -it craftcms_webphp php craft setup/security-key
+docker exec -it craftcms_webphp php craft setup/app-id
+
+docker exec -it craftcms_webphp php ./craft install/craft \
+                                            --interactive=0 \
+                                            --username=admin \
+                                            --site-url=http://localhost:8081 \
+                                            --site-name="Springlane Magazine" \
+                                            --password=password \
+                                            --language=en \
+                                            --email=backend@springlane.de
+
+
