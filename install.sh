d5b487f4c9f7cfacb181754f8bfab8bcef9dba83
David Thorn <david.thorn221278@googlemail.com>
2021-04-06 16:50:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-docker-compose up --build -d
+docker-compose down \
+                --remove-orphans \
+                --rmi=all # DO NOT UNCOMMENT - uncomment if you want a super-clean-install by removing all images.
 
-docker exec craft_cms_webphp composer install
+rm -rf src/vendor
+rm -rf src/.env
 
-docker exec -it craft_cms_webphp php craft setup
+cp src/.env.example src/.env
 
+docker-compose build --no-cache
+
+docker-compose up -d
+
+docker exec -it craftcms_webphp composer install
+
+docker exec -it craftcms_webphp php craft setup/security-key
+docker exec -it craftcms_webphp php craft setup/app-id
+
+docker exec -it craftcms_webphp php ./craft install/craft \
+                                            --interactive=0 \
+                                            --username=admin \
+                                            --site-url=http://localhost:8082 \
+                                            --site-name="Craft CMS - Initial" \
+                                            --password=password \
+                                            --language=en \
+                                            --email=david.thorn221278@googlemail.com
