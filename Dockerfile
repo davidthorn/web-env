e94bfcfeee4392022632b14d4733bd9888a1d727
David Thorn <david.thorn@springlane.de>
2021-05-08 08:51:02 +0200

    Renamed files, services

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ USER root
 RUN apt-get -y update \
     && apt-get install -y libicu-dev libfreetype6-dev\
     && docker-php-ext-configure intl \
-    && docker-php-ext-install intl
+    && docker-php-ext-install intl opcache
 
 RUN apt-get install -y \
         libpng-dev \
@@ -27,5 +27,11 @@ RUN docker-php-ext-configure gd \
 
 RUN docker-php-ext-install gd pdo pdo_mysql mysqli zip
 
+ADD --chown=www-data:www-data ./shopware /app
+VOLUME [ "/app" ]
 
+RUN usermod -u 1000 www-data && groupmod -g 1000 www-data 
+RUN chown -R www-data:www-data /app 
+
+USER www-data
 WORKDIR /app
--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+chmod +x shopware/shopware.php
+
 docker-compose build --no-cache
 
 docker-compose up -d
 
-docker exec -it sw_webphp chmod -R 755 custom/plugins
-docker exec -it sw_webphp chmod -R 755 engine/Shopware/Plugins/Community
-docker exec -it sw_webphp chmod -R 755 files
-docker exec -it sw_webphp chmod -R 755 media
-docker exec -it sw_webphp chmod -R 755 var
-docker exec -it sw_webphp chmod -R 755 web
+docker exec -it sw_php chmod -R 755 custom/plugins
+docker exec -it sw_php chmod -R 755 engine/Shopware/Plugins/Community
+docker exec -it sw_php chmod -R 755 files
+docker exec -it sw_php chmod -R 755 media
+docker exec -it sw_php chmod -R 755 var
+docker exec -it sw_php chmod -R 755 web
 
-docker exec -it sw_webphp make init
+docker exec -it sw_php make init
