27aa5eedb9ed78c7a10ac53d15f5a0c82ec24e90
Alex <global@djt.office.springlane.de>
2021-05-20 17:59:56 +0200

    Removed gitlab references and added sudo for docker commands

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose down \
+sudo docker-compose down \
                 --remove-orphans \
                 #--rmi=all # DO NOT UNCOMMENT - uncomment if you want a super-clean-install by removing all images.
 
@@ -10,27 +10,16 @@ rm -rf src/plugins
 
 cp src/.env.example src/.env
 
-if [ ! -d "src/plugins" ]; then
-    mkdir src/plugins
-fi
+sudo docker-compose build --no-cache
 
-if [ ! -d "src/plugins/basic-plugin" ]; then
-    git clone git@deploy.office.springlane.de:backend-modules/base-craft-plugin.git --branch=development src/plugins/basic-plugin
-    cd src
-    composer require springlane/basic-plugin
-    cd ..
-fi
+sudo docker-compose up -d
 
-docker-compose build --no-cache
+sudo docker exec -it craftcms_webphp composer install
 
-docker-compose up -d
+sudo docker exec -it craftcms_webphp php craft setup/security-key
+sudo docker exec -it craftcms_webphp php craft setup/app-id
 
-docker exec -it craftcms_webphp composer install
-
-docker exec -it craftcms_webphp php craft setup/security-key
-docker exec -it craftcms_webphp php craft setup/app-id
-
-docker exec -it craftcms_webphp php ./craft install/craft \
+sudo docker exec -it craftcms_webphp php ./craft install/craft \
                                             --interactive=0 \
                                             --username=admin \
                                             --site-url=http://localhost:8081 \
@@ -39,4 +28,3 @@ docker exec -it craftcms_webphp php ./craft install/craft \
                                             --language=en \
                                             --email=backend@springlane.de
 
-docker exec -it craftcms_webphp php craft plugin/install spl-custom-plugin-handle
